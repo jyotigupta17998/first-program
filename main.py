def39d82b17d52cd67c48badc58d6d5cd0bbbd1e
Jyoti Gupta <63253312+jyotigupta17998@users.noreply.github.com>
2023-03-27 10:34:41 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+num1 = int(input("Enter the first number= "))
+num2 = int(input("Enter the Second number= "))
+add = num1 + num2
+print("the addition of", num1 , "and", num2,"is", add)
+sub = num1 - num2
+print("the substraction of", num1 , "and", num2,"is", sub)
+Mul = num1 * num2
+print("the Multiplication of", num1 , "and", num2,"is", Mul)
+Div = num1 / num2
+print("the Division of", num1 , "and", num2,"is", Div)
+Floor = num1 // num2
+print("the Floor Division of", num1 , "and", num2,"is" ,Floor)
+Exp = num1 ** num2
+print("the Exponenet of", num1 , "and", num2,"is", Exp)
